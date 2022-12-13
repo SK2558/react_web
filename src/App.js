a586cb97a5447040eb1f51452b02de0409360160
Shoaib Pathan <shoaibpathan812@gmail.com  git config --global user.email>
2022-12-13 18:51:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Restaurant from "./Components/Basics/Restaurant";
+
+
+
+const App = () => {
+  return <div> <Restaurant/></div>      
+          
+};
+
+export default App;
